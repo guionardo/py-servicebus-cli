0e6bb7527936aa73752b472ca91bdbcf2e8ee61c
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2021-09-17 01:30:34 -0300

    Added peek functions

--- src/cli/tool_peek.py
+++ src/cli/tool_peek.py
@@ -3,3 +3,13 @@ def tool_peek(args):
         _tool_peek_queue(args)
     elif args.topic:
         _tool_peek_topic(args)
+
+
+def _tool_peek_queue(args):
+    # TODO: Implementar tool peek queue
+    raise NotImplementedError("tool_peek_queue")
+
+
+def _tool_peek_topic(args):
+    # TODO: Implementar tool peek topic
+    raise NotImplementedError("tool_peek_topic")
